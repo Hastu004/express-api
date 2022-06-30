a58adce3c9f351bb63e64929513692bc061ab195
Hernán Astudillo <hernan.astudillo.r@gmail.com>
2022-06-30 14:37:16 -0400

    sda

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const swaggerSpec = {
         },
         servers: [
             {
-                url: "https://localhost"
+                url: "https://express-api-production-0d8b.up.railway.app"
             }
         ]
     },
